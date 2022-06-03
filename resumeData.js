54a029bc6683abe7418b6d5e83db1f947c5ae303
TAMMIREDDYSIDDU0 <93256084+TAMMIREDDYSIDDU0@users.noreply.github.com>
2022-06-03 16:12:02 +0530

    Add files via upload

---
+++ resumeData.js
@@ -0,0 +1,134 @@
+let resumeData = {
+    "imagebaseurl":"https://picsum.photos/200/300",
+    "name": "TAMMIREDDY SIDDA SAI DATTA VENKAT",
+    "role": "Frontend developer",
+    "linkedinId":"https://www.linkedin.com/in/tammireddysiddu/",
+    "email":"siddutssd@gmail.com",
+    "roleDescription": `Experienced with a demonstrated history of working in the information technology and services industry. Skilled in JavaScript, Reactjs, Nodejs, HTML and CSS . `,
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/tammireddysiddu/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/TAMMIREDDYSIDDU0",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"Email",
+          "url":"mailto:siddutssd@gmail.com",
+          "className":"fa fa-envelope"
+        },
+      ],
+    "aboutme":`I am currently working as Frontend developer in Cubastion Consulting Pvt. Ltd,enthusiastic
+                software developer interested to build things.Specialized in javascript tech stack like Reactjs,Nodejs,`,
+    "address":"Andhrapradesh,India",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"Chandigarh University",
+        "specialization":"Btech in Computer science and Engineering Specialization in Internet Of Things",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2022",
+        'Score':'7.0 CGPA'
+
+      },
+      {
+        "UniversityName":"Narayana Junior College",
+        "specialization":"10+2 in MPC",
+        "MonthOfPassing":"April",
+        "YearOfPassing":"2018",
+        'Score':'90.5%'
+
+
+      },
+      {
+        "UniversityName":"Bharatiya Vidya Bhavan's",
+        "specialization":"10th Standard",
+        "MonthOfPassing":"April",
+        "YearOfPassing":"2016",
+        'Score':'10 CGPA'
+
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Cubastion Consulting Pvt. Ltd",
+        "specialization":"Frontend developer",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2022",
+        "Achievements": ["Created multiple web apps starting with requirements gathering, designing, developing, refactoring, and code implementation",
+                         "Implemented the redesigned user dashboard with better UX and performance using reactjs"  ,
+                          "Designed ,developed and hosted Nodejs,React apps with Nginx",
+                           ]
+
+      },
+
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+
+      {
+        "skillname": "Reactjs",
+        level: 65
+      },
+      {
+        "skillname": "Reduxjs",
+        level: 60
+      },
+      {
+        "skillname": "Nodejs",
+        level: 60
+      },
+      {
+        "skillname": "Expressjs",
+        level: 62
+      },
+      ,
+      {
+        "skillname": "HTML5",
+        level: 75
+      },
+      {
+        "skillname": "CSS",
+        level: 65
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"Shopg",
+        "description":`Revamped the whole app according to the customer's need
+        integrated SMS and Push notification
+        created RESTful api endpoints
+        Tools:
+        Reactjs,Nodejs,Expressjs,reduxjs,Postgresql,HTML5,CSS3,NGINX,firebase,Aws`,
+        "imgurl":"images/shop.jpg"
+      },
+      {
+        "name":"Skoolnet",
+        "description":`developed screens using react-native for IOS/Android
+        Tracked and solved tickets for defects on bug reporting tool (JIRA)
+        Tools: React Native`,
+        "imgurl":"images/skoolnet.jpg"
+      },
+      {
+        "name":"Dawaadost",
+        "description":`created a vendor App for IOS/Android using react native
+        developed and deployed the frontend and backend of the website
+        Tools: Reactjs,Nodejs,Expressjs,reduxjs,Postgresql,HTML5,CSS3,Nginx,Ant Design`,
+        "imgurl":"images/medicine.jpg"
+      },
+      {
+        "name":"Mai web",
+        "description":`created pixel perfect responsive Web pages for provided screens
+        using Reactjs
+        Consumed RESTful api for rendering data using Axios
+        Tools: Reactjs,reduxjs,HTML5,CSS3`,
+        "imgurl":"images/parenting.jpg"
+      }
+    ],
+  }
+
+  export default resumeData
